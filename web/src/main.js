c70f804b3d8ddb2ac1861c46c7edc3c829c60884
Thiago Cabral <thiago.cabral@immail.ca>
2024-01-28 19:52:54 -0300

    Versioned .env file; Error message module created; Comments file created;

--- web/src/main.js
+++ web/src/main.js
@@ -4,12 +4,17 @@ import VuetifyUseDialog from 'vuetify-use-dialog'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
 
+import Vue3Toasity from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
 import App from './App.vue'
 
 loadFonts()
 
 const app = createApp(App);
+
 app.use(vuetify);
+
 app.use(VuetifyUseDialog, {
   confirmDialog: {
     title: 'Tem certeza?',
@@ -20,5 +25,9 @@ app.use(VuetifyUseDialog, {
     closeButtonText: 'Fechar'
   }
 });
+
+app.use(Vue3Toasity, { autoClose: 3000 });
+
 app.use(router);
+
 app.mount('#app');
